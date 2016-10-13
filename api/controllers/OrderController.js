404946089f748b597bc429254650eaf2b5b4d2ff
Roman Hromenkov <roman.hromenkov@gmail.com>
2016-10-13 15:41:01 +0300

    Trying to add function that allow to change order owner in order edit window, but not luck yet :)

--- api/controllers/OrderController.js
+++ api/controllers/OrderController.js
@@ -24,6 +24,55 @@ module.exports = {
             /* res.redirect('/order/'); */
             res.redirect('/customer/show/' + order.owner);
         });
+    },
+
+    show: function (req, res, next) {
+        Order.findOne(req.param('id')).populateAll().exec(function (err, order) {
+            if (err) return next(err);
+            if (!order) return next();
+
+            res.view({
+                order: order
+            });
+        });
+    },
+
+    index: function (req, res, next) {
+        Order.find(function foundOrders(err, orders) {
+            if (err) return next(err);
+
+            res.view({
+                orders: orders
+            });
+        });
+    },
+
+    edit: function (req, res, next) {
+        Order.findOne(req.param('id'), function foundOrder(err, order) {
+            if (err) return next(err);
+            if (!order) return next();
+
+            res.view({
+                order: order
+            });
+        });
+    },
+
+    update: function (req, res, next) {
+        Order.update(req.param('id'), req.params.all(), function orderUpdated(err) {
+            if (err) {
+                return res.redirect('/order/edit/' + req.param('id'));
+            }
+
+            res.redirect('/order/');
+            /* res.redirect('/customer/show/' + req.param('id')); */
+        });
+    },
+
+    destroy: function (req, res, next) {
+        Order.destroy(req.param('id')).exec(function () {
+            res.redirect('/order/');
+        });
     }
 
 };
